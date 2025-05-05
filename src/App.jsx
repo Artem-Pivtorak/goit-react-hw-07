df3f4337987e1b1439dc858e088aeb92ac8d958d
Pivtorak Artem <mulbrry.digital@gmail.com>
2025-05-05 15:06:57 +0300

    edityng error

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import ContactForm from './components/ContactForm/ContactForm';
 import { ContactList } from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
 
-import SearchBox from './components/SearchBox/SearchBox';
 import css from './App.module.css';
 
 import { fetchContacts, addContact, deleteContact } from './redux/contactsOps';
-import { selectContacts, selectError, selectLoading } from './redux/contactsSlice';
+import { selectFilteredContacts, selectError, selectLoading } from './redux/contactsSlice';
 
 const App = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
+  const contacts = useSelector(selectFilteredContacts);
   const isLoading = useSelector(selectLoading);
   const error = useSelector(selectError);
-
-  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -30,18 +28,14 @@ const App = () => {
     dispatch(deleteContact(id));
   };
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm onAdd={handleAddContact} />
-      <SearchBox filter={filter} onChange={setFilter} />
+      <Filter />
       {isLoading && <p>Loading contacts...</p>}
       {error && <p>Error: {error}</p>}
-      <ContactList contacts={filteredContacts} onDelete={handleDelete} />
+      <ContactList contacts={contacts} onDelete={handleDelete} />
     </div>
   );
 };
